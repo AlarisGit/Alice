845f90d8adac10da22a81a98289a34d6c935c2d7
Sergey Alekseev <sergey@alarislabs.com>
2023-01-28 19:06:16 +0300

    Installation directory changed to /usr/lib/alice

--- client/core/prepare.sh
+++ client/core/prepare.sh
@@ -1,19 +1,24 @@
+#!/bin/bash
+
 set -v
+set -e
+
 HOME=/home/alice
+INSTALL_DIR=/usr/lib/alice
 PYTHON_MAJOR_VERSION=3.10
 PYTHON_MINOR_VERSION=9
 PYTHON_VERSION=$PYTHON_MAJOR_VERSION.$PYTHON_MINOR_VERSION
-PROFILE=$HOME/.bash_profile
-PYTHON_HOME=$HOME/python
-OPENSSL_BASE=/usr/local
-OPENSSL_HOME=$OPENSSL_BASE/openssl
-LIBDIR=$PYTHON_HOME/lib
+PYTHON_HOME=$INSTALL_DIR/python
+PYTHON_LIB_DIR=$PYTHON_HOME/lib
+LIB_DIR=$INSTALL_DIR
 
 INSTALL_SCRIPT_DIR=`pwd`
-SETENV=$HOME/setenv.sh
+JAVA_ENV=$HOME/java.env
+ENV=$HOME/alice.env
+
 
 : '
-sudo yum install -y mc make gcc gcc-c++ wget zlib zlib-devel libffi-devel maven perl
+sudo yum install -y mc make gcc gcc-c++ wget zlib zlib-devel libffi-devel maven perl openssl java
 
 cd $HOME
 
@@ -26,6 +31,8 @@ rm -rf $SSL_SOURCE_DIR
 tar -xzf $FILE
 cd $SSL_SOURCE_DIR
 ./config
+#OPENSSL_BASE=/usr/local
+#OPENSSL_HOME=$OPENSSL_BASE/openssl
 # --prefix=$OPENSSL_HOME --openssldir=$OPENSSL_HOME/ssl
 make
 sudo rm -rf $OPENSSL_HOME
@@ -36,12 +43,18 @@ make install
 #sudo make install
 
 
+#export LD_LIBRARY_PATH=$OPENSSL_HOME/lib:$LIB_DIR
+'
 
-#export LD_LIBRARY_PATH=$OPENSSL_HOME/lib:$LIBDIR
+
+ME=`whoami`
+sudo mkdir -p $INSTALL_DIR
+sudo chown -R $ME $INSTALL_DIR
 
 cd $HOME
-rm -rf $PYTHON_HOME
 rm -rf Python-$PYTHON_VERSION
+
+rm -rf $PYTHON_HOME
 mkdir -p $PYTHON_HOME
 
 FILE=Python-$PYTHON_VERSION.tgz
@@ -52,23 +65,22 @@ tar -xzf $FILE
 
 cd Python-$PYTHON_VERSION
 find . -type d | xargs chmod 0755
-./configure --prefix=$PYTHON_HOME --enable-optimizations --with-openssl=$OPENSSL_BASE --with-openssl-rpath=auto --enable-shared
+#./configure --prefix=$PYTHON_HOME --enable-optimizations --with-openssl=$OPENSSL_BASE --with-openssl-rpath=auto --enable-shared
+./configure --prefix=$PYTHON_HOME --enable-optimizations --enable-shared
 make
 
-'
 
-grep -v -i python $PROFILE > ${PROFILE}.backup
-cat ${PROFILE}.backup > $PROFILE
-echo "export PATH=$PYTHON_HOME/bin:$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin" >> $PROFILE
-#echo "export PYTHONPATH=$LIBDIR:$HOME/python/Python-$PYTHON_VERSION" >> $PROFILE
-echo "export PYTHONPATH=$LIBDIR" >> $PROFILE
-echo "export PYTHONPATH=$LIBDIR" >> $SETENV
-echo "export LD_LIBRARY_PATH=$LIBDIR" >> $PROFILE
-echo "export LD_LIBRARY_PATH=$LIBDIR" >> $SETENV
-echo "export LD_PRELOAD=$HOME/python/lib/python$PYTHON_MAJOR_VERSION.so.1.0" >> $SETENV
-chmod 755 $SETENV
+#grep -v -i python $PROFILE > ${PROFILE}.backup
+#cat ${PROFILE}.backup > $PROFILE
+echo "export PATH=$PYTHON_HOME/bin:$HOME/.local/bin:$HOME/bin:/usr/local/bin:/usr/bin:/usr/local/sbin:/usr/sbin" > $ENV
+echo "export PYTHONPATH=$LIB_DIR" >> $ENV
+echo "export PYTHONPATH=$LIB_DIR" > $JAVA_ENV
+echo "export LD_LIBRARY_PATH=$LIB_DIR:$PYTHON_LIB_DIR" >> $ENV
+echo "export LD_LIBRARY_PATH=$LIB_DIR:$PYTHON_LIB_DIR" >> $JAVA_ENV
+echo "export LD_PRELOAD=$PYTHON_LIB_DIR/libpython$PYTHON_MAJOR_VERSION.so.1.0" >> $JAVA_ENV
 
-source $PROFILE
+
+source $ENV
 
 env
 
@@ -78,7 +90,6 @@ echo $INSTALL_SCRIPT_DIR
 
 cd $INSTALL_SCRIPT_DIR
 
-
 python$PYTHON_MAJOR_VERSION -m pip install --upgrade pip
 python$PYTHON_MAJOR_VERSION -m pip install wheel
 python$PYTHON_MAJOR_VERSION -m pip install ordered-set
@@ -86,16 +97,16 @@ python$PYTHON_MAJOR_VERSION -m pip install nuitka
 python$PYTHON_MAJOR_VERSION -m pip install requests
 
 
-mkdir -p $LIBDIR
-rm -f $LIBDIR/libalice*.so*
+mkdir -p $LIB_DIR
+rm -f $LIB_DIR/libalice*.so*
 
 
 python$PYTHON_MAJOR_VERSION -m nuitka --module src/libalice2.py --include-package=requests --include-package=ssl --include-package=urllib3 --include-package=json
 rm -rf libalice2.build libalice2.pyi
 chmod 755 libalice2.cpython*.so
-mv -f libalice2.cpython*.so $LIBDIR/libalice2.so
+mv -f libalice2.cpython*.so $LIB_DIR/libalice2.so
 
-gcc -g -shared -pthread -fPIC -fwrapv -O2 -L$LIBDIR -Wl,--strip-all -Wall -fno-strict-aliasing -I$PYTHON_HOME/include/python$PYTHON_MAJOR_VERSION src/embed.c -o $LIBDIR/libalice.so
+gcc -g -shared -pthread -fPIC -fwrapv -O2 -L$LIB_DIR -Wl,--strip-all -Wall -fno-strict-aliasing -I$PYTHON_HOME/include/python$PYTHON_MAJOR_VERSION src/embed.c -o $LIB_DIR/libalice.so
 
-gcc -g -fPIC -fwrapv -O2 -Wall -L$LIBDIR -lpython$PYTHON_MAJOR_VERSION -lalice -I$PYTHON_HOME/include/python$PYTHON_MAJOR_VERSION test/test.c -o test.x
+gcc -g -fPIC -fwrapv -O2 -Wall -L$PYTHON_LIB_DIR -lpython$PYTHON_MAJOR_VERSION -L$LIB_DIR -lalice -I$PYTHON_HOME/include/python$PYTHON_MAJOR_VERSION test/test.c -o test.x
 
